7b5c55eb7599008e65c5defca4542edd1704c1dd
Gdominguezborines <Gdominguezborines@danielcastelao.org>
2020-10-20 11:46:07 +0200

    segundo commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-  System.out.println("hola que tal estas");
+  System.out.println("hola que tal estas bien??");
 
    }
 }
